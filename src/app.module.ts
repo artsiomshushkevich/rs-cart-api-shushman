6e231ae2051793a09ece49bf342b47dfb61a0a9f
Artsiom Shushkevich <artsiom_shushkevich@epam.com>
2023-04-23 15:48:04 +0300

    [task-9] Configure cart service as standard service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { CartModule } from './cart/cart.module';
 import { OrderModule } from './order/order.module';
 import { ProductModule } from './product/product.module';
 
 @Module({
-    imports: [CartModule, OrderModule, ProductModule],
+    imports: [ConfigModule.forRoot(), CartModule, OrderModule, ProductModule],
     controllers: [AppController],
     providers: []
 })
